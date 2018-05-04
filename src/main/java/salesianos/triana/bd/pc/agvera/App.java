efdfbf7bcd6e4f21141e69924dadac82de9ed189
Alejandro García Vera <agvera@aulas.triana>
2018-05-04 11:00:02 +0200

    Añadido carpeta

---
+++ src/main/java/salesianos/triana/bd/pc/agvera/App.java
@@ -0,0 +1,19 @@
+package salesianos.triana.bd.pc.agvera;
+
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.impl.factory.Lists;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ImmutableList<String> pentateuco = Lists.immutable.of("Génesis", "Éxodo", "Levítico", "Números",
+				"Deuteronomio");
+		
+		for (String s : pentateuco) {
+			System.out.println(s);
+		}
+	}
+}
